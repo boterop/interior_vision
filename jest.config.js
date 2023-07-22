86ed231a2940f9d6773f94e75d3874b13195ab67
Santiago Botero <98826652+boterop@users.noreply.github.com>
2023-07-21 21:41:00 -0500

    Fix Async Storage test issues

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   preset: 'react-native',
+  setupFiles: ["./tests/jestSetup.js"],
+  transform: {
+    "^.+\\.[t|j]sx?$": "babel-jest"
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!((jest-)?react-native|@fortawesome|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
+  ],
+  collectCoverage: true
 };
