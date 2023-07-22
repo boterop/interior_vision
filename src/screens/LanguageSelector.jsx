19284eca6c5e69a4eb3cebabd43de308f8c25405
Santiago Botero <98826652+boterop@users.noreply.github.com>
2023-07-22 13:48:49 -0500

    Improve SelectBox component

--- src/screens/LanguageSelector.jsx
+++ src/screens/LanguageSelector.jsx
@@ -1,36 +1,10 @@
 import React, {useRef, useState} from 'react';
-import {FlatList, Image, Pressable, Text, View} from 'react-native';
+import {View} from 'react-native';
 import {Button, SelectBox} from '../components';
 import {StorageService} from '../services';
 
 const getLanguageByCode = (values, code) =>
   values.filter(lang => lang.code === code)[0];
-
-const LangDescription = ({lang, onSetLanguage}) => (
-  <Pressable
-    className="flex-row w-full h-14 mb-2"
-    onPress={() => onSetLanguage(lang)}>
-    <View className="justify-center ">
-      <Image className="h-full w-[50px] mr-3" source={lang.icon} />
-    </View>
-    <View className="justify-center h-full">
-      <Text className="capitalize w-full text-2xl text-black font-black">
-        {lang.name}
-      </Text>
-    </View>
-  </Pressable>
-);
-
-const Modal = ({values, onSetLanguage}) => (
-  <View className="items-left bg-light-base mt-5 mb-28 p-4 w-64 h-44 rounded-3xl border-2 border-black">
-    <FlatList
-      data={values}
-      renderItem={({item}) => (
-        <LangDescription lang={item} onSetLanguage={onSetLanguage} />
-      )}
-    />
-  </View>
-);
 
 const LanguageSelector = ({translate, currentLanguage, onChangeLanguage}) => {
   const values = [
@@ -40,7 +14,6 @@ const LanguageSelector = ({translate, currentLanguage, onChangeLanguage}) => {
   ];
 
   const [language, setLanguage] = useState(values[0]);
-  const [isVisible, setIsVisible] = useState(false);
   const isInitialMount = useRef(true);
 
   const onSelectLanguage = lang => {
@@ -49,7 +22,6 @@ const LanguageSelector = ({translate, currentLanguage, onChangeLanguage}) => {
   };
 
   const onAcceptSelection = () => {
-    setIsVisible(false);
     StorageService.save('language', currentLanguage);
   };
 
@@ -61,22 +33,16 @@ const LanguageSelector = ({translate, currentLanguage, onChangeLanguage}) => {
   }
 
   return (
-    <Pressable
-      className="relative items-center h-full w-full"
-      onPress={() => setIsVisible(false)}>
+    <View className="relative items-center h-full w-full">
       <View className="bottom-0 absolute flexflex-wrap items-center justify-between h-[65%] w-full mb-10">
         <SelectBox
-          onPress={() => {
-            setIsVisible(!isVisible);
-          }}
+          values={values}
+          onSelectLanguage={onSelectLanguage}
           icon={language.icon}
         />
-        {isVisible ? (
-          <Modal values={values} onSetLanguage={onSelectLanguage} />
-        ) : null}
         <Button onPress={onAcceptSelection} text={translate('accept')} />
       </View>
-    </Pressable>
+    </View>
   );
 };
 
