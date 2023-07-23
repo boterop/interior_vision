d045749c4a9b5bf0bb7163323c7bd5e7d54d576b
Santiago Botero <98826652+boterop@users.noreply.github.com>
2023-07-23 12:43:24 -0500

    Creating chat view

--- src/components/ChatView.jsx
+++ src/components/ChatView.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import {FlatList, Pressable, Text, View} from 'react-native';
 import Triangle from './Triangle';
 
+const SYSTEM_ROLE = 'system';
+const USER_ROLE = 'user';
+
 const capitalize = text => text.charAt(0).toUpperCase() + text.slice(1);
 
 const ChatItem = ({chatObject, onSelectMessage}) => (
-  <Pressable onPress={onSelectMessage}>
-    <Text className="text-dark-dark w-full text-xl font-black">
-      {capitalize(chatObject)}
+  <Pressable className="mb-5" onPress={onSelectMessage}>
+    <Text
+      className={`text-dark-dark w-full text-xl font-black ${
+        chatObject.role === USER_ROLE ? 'text-right' : ''
+      }`}>
+      {capitalize(chatObject.content)}
     </Text>
   </Pressable>
 );
@@ -16,18 +22,29 @@ const ChatView = ({classname}) => (
   <View className={`w-full pb-12 ${classname}`}>
     <Triangle
       classname="absolute bottom-0 left-20 rotate-45 border-t-[100px] border-x-[40px]"
-      color="#E0EDFF"
+      color="light-base"
     />
-    <View className="bg-light-base rounded-3xl items-center w-full h-full mb-16 p-3">
+    <View className="bg-light-base rounded-3xl items-center w-full h-full mb-16 p-3 pt-5">
       <View className="h-full w-full">
         <FlatList
-          data={['hola como estas?']}
-          renderItem={({item}) => (
-            <ChatItem
-              chatObject={item}
-              onSelectMessage={() => console.log('TODO')}
-            />
-          )}
+          data={[
+            {role: 'system', content: 'You are a helpful assistant.'},
+            {role: 'user', content: 'Who won the world series in 2020?'},
+            {
+              role: 'assistant',
+              content: 'The Los Angeles Dodgers won the World Series in 2020.',
+            },
+            {role: 'user', content: 'Where was it played?'},
+          ]}
+          renderItem={({item}) => {
+            if (item.role !== SYSTEM_ROLE)
+              return (
+                <ChatItem
+                  chatObject={item}
+                  onSelectMessage={() => console.log('TODO')}
+                />
+              );
+          }}
         />
       </View>
     </View>
--- src/components/Triangle.jsx
+++ src/components/Triangle.jsx
@@ -3,7 +3,7 @@ import {View} from 'react-native';
 
 const Triangle = ({classname, color = 'black'}) => (
   <View
-    className={`w-0 h-0 border-solid border-[${color}] border-l-[12px] border-l-transparent border-t-[20px] border-r-[12px] border-r-transparent border-b-transparent ${classname}`}
+    className={`w-0 h-0 border-solid border-${color} border-l-[12px] border-l-transparent border-t-[20px] border-r-[12px] border-r-transparent border-b-transparent ${classname}`}
   />
 );
 
