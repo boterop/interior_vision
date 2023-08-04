6145126ec2cf66953317d3f4486623dfb21ca771
Santiago Botero <98826652+boterop@users.noreply.github.com>
2023-08-04 15:12:55 -0500

    Updating API comunication (#20)

--- src/services/API.js
+++ src/services/API.js
@@ -1,23 +1,37 @@
 import axios from 'axios';
-import {API_URL, TEST_API_URL} from '@env';
+import {API_URL, TEST_API_URL, API_KEY} from '@env';
 
 const post = (uri, body = {}) =>
   axios
     .post((__DEV__ ? TEST_API_URL : API_URL) + uri, body, {
       headers: {
         'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + API_KEY,
       },
     })
     .then(response => response.data);
 
 const API = {
   createAssistant: language => post('/create-assistant', {language: language}),
-  getMemory: assistantID => post('/get-memory', {assistant_id: assistantID}),
-  ask: (question, assistantID) =>
-    post('/ask', {assistant_id: assistantID, message: question}),
-  view: assistantID => post('/view', {assistant_id: assistantID}),
-  cleanMemory: (assistantID, language) =>
-    post('/clean-memory', {assistant_id: assistantID, language: language}),
+  getMemory: (assistantID, assistantKey) =>
+    post('/get-memory', {
+      assistant_id: assistantID,
+      assistant_key: assistantKey,
+    }),
+  ask: (question, assistantID, assistantKey) =>
+    post('/ask', {
+      assistant_id: assistantID,
+      assistant_key: assistantKey,
+      message: question,
+    }),
+  view: (assistantID, assistantKey) =>
+    post('/view', {assistant_id: assistantID, assistant_key: assistantKey}),
+  cleanMemory: (assistantID, assistantKey, language) =>
+    post('/clean-memory', {
+      assistant_id: assistantID,
+      assistant_key: assistantKey,
+      language: language,
+    }),
 };
 
 export default API;
